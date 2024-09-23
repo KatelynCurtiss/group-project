304ac7999ef683c001c523ec1aa5135e0741775c
Katelyn Curtiss <153852200+KatelynCurtiss@users.noreply.github.com>
2024-09-23 11:16:27 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+# Katelyn Curtiss
+# Setember 23 2024
+# Group project
+
+#  /\_/\
+# (=^.^=)
+#  (")(")
+
+
+
+# Input
+num1 = float(input("Please enter the base: "))
+num2 = float(input("Please enter the height: "))
+
+
+# Process
+# Practice useing all the Arithmetic Operators!^.^
+total = num1 * num2
+
+
+
+# Output
+print("The sum of the right triangle is " + str (total) + ".")
+
